a9a9b4390e5e56bfdd1f376c5d82a75458b1c6c4
mattdd50 <86108751+mattdd50@users.noreply.github.com>
2025-03-19 12:59:20 +0000

    Delete .gitignore directory

--- .gitignore/firebase-config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyColT7W4g9qO1gj1T4F4IAk3YcIKc26b8U",
-  authDomain: "legoland-26db2.firebaseapp.com",
-  databaseURL: "https://legoland-26db2-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "legoland-26db2",
-  storageBucket: "legoland-26db2.firebasestorage.app",
-  messagingSenderId: "401569741885",
-  appId: "1:401569741885:web:0daeed43022ffdab7ebe54"
-};
